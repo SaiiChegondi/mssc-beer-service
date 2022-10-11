86019d0256fc31d35e46422488ff11035f14056a
SaiiChegondi <chegondinagasai@gmail.com>
2022-10-11 14:18:27 +0530

    adding models #2

---
+++ src/main/java/guru/springframework/msscbeerservice/model/BeerDto.java
@@ -0,0 +1,40 @@
+package guru.springframework.msscbeerservice.model;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDto {
+
+	private UUID id;
+	private Integer verison;
+	
+	private OffsetDateTime createdDate;
+	private OffsetDateTime lastModifiedDate;
+	
+	private String beerName;
+	
+	private BeerStyleEnum beerStyle;
+	
+	private  String upc;
+	
+	
+	private BigDecimal price;
+	
+	private Integer quantityOnHand;
+	
+	
+	
+	
+	
+}
---
+++ src/main/java/guru/springframework/msscbeerservice/model/BeerPagedList.java
@@ -0,0 +1,15 @@
+package guru.springframework.msscbeerservice.model;
+
+import java.util.List;
+
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.domain.PageImpl;
+
+public class BeerPagedList extends PageImpl<BeerDto>{
+
+	public BeerPagedList(List<BeerDto> content) {
+		super(content);
+		// TODO Auto-generated constructor stub
+	}
+
+}
